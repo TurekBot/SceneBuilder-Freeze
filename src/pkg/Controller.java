9e2a80d88b0dd680634ab8f61610e052f0cd44ab
Bradley Turek <TurekBot@gmail.com>
2017-10-24 17:07:58 -0600

    First Commit. Just a simple example. Added a gitignore (though I may have already committed things it contains)

--- src/pkg/Controller.java
+++ src/pkg/Controller.java
@@ -1,4 +1,23 @@
 package pkg;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private ComboBox<String> comboBox;
+
+    @FXML
+    private TextField textField;
+
+    @FXML
+    private void initialize() {
+        for (int i = 0; i < 5; i++) {
+            comboBox.getItems().add("Item " + i);
+        }
+
+    }
+
 }
